4fb5e54de56fa30d8bf44d4092442f31b60136ce
David Robinson <david.robinson@logicstudio.net>
2020-03-25 16:07:17 -0500

    Componentes, directivas

---
+++ 05-miscelaneos/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { CssComponent } from './components/css/css.component';
+import { ClasesComponent } from './components/clases/clases.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NgStyleComponent,
+    CssComponent,
+    ClasesComponent,
+    HomeComponent,
+    UsuarioComponent,
+    UsuarioNuevoComponent,
+    UsuarioEditarComponent,
+    UsuarioDetalleComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 05-miscelaneos/src/app/components/clases/clases.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clases',
+  templateUrl: './clases.component.html',
+  styles: []
+})
+export class ClasesComponent implements OnInit {
+
+  alerta: string = 'alert-danger';
+  loading: boolean;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ejecutar() {
+    this.loading = true;
+    setTimeout(() => this.loading = false, 3000);
+  }
+
+}
